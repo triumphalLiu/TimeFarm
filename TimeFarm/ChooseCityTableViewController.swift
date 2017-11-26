ffe4b9e8565ca12be614cbad9b98e5c5b64f0d77
Liu <liu@triumphal.cn>
2017-11-26 16:55:20 +0800

    完成导航栏和section栏
    下一步 搜索功能

--- TimeFarm/ChooseCityTableViewController.swift
+++ TimeFarm/ChooseCityTableViewController.swift
@@ -23,7 +23,7 @@ class ChooseCityTableViewController:UITableViewController{
             array.append(str)
         }
         array.sort()
-        array.insert("定位城市", at: 0)
+        array.insert("定位", at: 0)
         return array
     }()
     
@@ -31,6 +31,7 @@ class ChooseCityTableViewController:UITableViewController{
         super.viewDidLoad()
         print(cityDic)
         print(titleArray)
+        tableView.sectionIndexBackgroundColor = UIColor.clear
     }
     
     override func didReceiveMemoryWarning() {
@@ -70,5 +71,28 @@ class ChooseCityTableViewController:UITableViewController{
         let cell = tableView.cellForRow(at: indexPath) as! ChooseCityTableViewCell
         print("点击了 \(cell.cityNameLabel.text)")
         //把值返回给上一层
+    }
+    
+    //右侧导航栏
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return titleArray
+    }
+    
+    //secton头
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 20))
+        let title = UILabel(frame: CGRect(x: 12, y: 0, width: UIScreen.main.bounds.width - 12, height: 20))
+        var titleArr = titleArray
+        titleArr[0] = "定位城市"
+        title.text = titleArr[section]
+        title.textColor = UIColor.white
+        title.font = UIFont.boldSystemFont(ofSize: 15)
+        view.addSubview(title)
+        view.backgroundColor = UIColor.gray
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
     }
 }
