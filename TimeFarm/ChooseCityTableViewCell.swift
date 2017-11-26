fb1cc047abfffd1aad97143142737e3dca8d339d
Liu <liu@triumphal.cn>
2017-11-26 16:35:28 +0800

    选择城市ui完成
    下一步 添加索引菜单 完成搜索功能

---
+++ TimeFarm/ChooseCityTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ChooseCityTableViewCell.swift
+//  TimeFarm
+//
+//  Created by apple on 2017/11/26.
+//  Copyright © 2017年 liu. All rights reserved.
+//
+
+import UIKit
+
+class ChooseCityTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+}
--- TimeFarm/ChooseCityTableViewController.swift
+++ TimeFarm/ChooseCityTableViewController.swift
@@ -9,31 +9,28 @@
 import UIKit
 class ChooseCityTableViewController:UITableViewController{
     
-    @IBOutlet var CityTableView: UITableView!
+    //加载城市数据
+    lazy var cityDic: [String: [String]] = { () -> [String : [String]] in
+        let path = Bundle.main.path(forResource: "cities", ofType: "plist")
+        let dic = NSDictionary(contentsOfFile: path ?? "") as? [String: [String]]
+        return dic ?? [:]
+    }()
     
-    var webs:NSArray?
-    
-    private func addCitiesToTableView() {
-        webs = NSArray(objects: "hangge.com","baidu.com","google.com","163.com","qq.com")
-        self.tableView!.delegate = self
-        self.tableView!.dataSource = self
-        self.tableView!.register(UITableViewCell.self, forCellReuseIdentifier: "cell0")
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return webs!.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell0", for: indexPath) as UITableViewCell
-        let url = webs![indexPath.row] as! String
-        cell.textLabel?.text = url
-        return cell
-    }
+    //加载标题
+    lazy var titleArray: [String] = { () -> [String] in
+        var array = [String]()
+        for str in self.cityDic.keys {
+            array.append(str)
+        }
+        array.sort()
+        array.insert("定位城市", at: 0)
+        return array
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addCitiesToTableView()
+        print(cityDic)
+        print(titleArray)
     }
     
     override func didReceiveMemoryWarning() {
@@ -42,5 +39,36 @@ class ChooseCityTableViewController:UITableViewController{
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return titleArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section > 0 {
+            let key = titleArray[section]
+            return cityDic[key]!.count - 3
+        }
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChooseCityTableViewCell", for: indexPath) as! ChooseCityTableViewCell
+        let key = titleArray[indexPath.section]
+        if indexPath.section == 0 {
+            cell.cityNameLabel!.text = "当前定位"
+        }
+        else {
+            cell.cityNameLabel.text = cityDic[key]?[indexPath.row]
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        let cell = tableView.cellForRow(at: indexPath) as! ChooseCityTableViewCell
+        print("点击了 \(cell.cityNameLabel.text)")
+        //把值返回给上一层
     }
 }
