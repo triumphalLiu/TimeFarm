66aa6b082ebe590e57a4a16ac4c95ab5c9f468b5
Liu <liu@triumphal.cn>
2017-11-28 20:08:36 +0800

    选择城市及搜索界面完成
    下一步 设置界面完善

--- TimeFarm/ChooseCity/ChooseCityViewController.swift
+++ TimeFarm/ChooseCity/ChooseCityViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 class ChooseCityViewController:UIViewController{
     
+    
     @IBOutlet weak var tableView: UITableView!
     //加载城市数据
     lazy var cityDic: [String: [String]] = { () -> [String : [String]] in
         let path = Bundle.main.path(forResource: "cities", ofType: "plist")
         let dic = NSDictionary(contentsOfFile: path ?? "") as? [String: [String]]
         return dic ?? [:]
+    }()
+    
+    lazy var cityArray: [String] = { () -> [String] in
+        var arr = [String]()
+        for (keys, values) in self.cityDic{
+            arr.append(contentsOf: values)
+        }
+        return arr
     }()
     
     //加载标题
@@ -28,12 +37,32 @@ class ChooseCityViewController:UIViewController{
         return array
     }()
     
+    //搜索结果
+    var resultArray : [String] = [String](){
+        didSet {self.tableView.reloadData()}
+    }
+    
+    //搜索控制器
+    var ChooseCitySearchController = UISearchController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(titleArray)
         tableView.delegate = self
         tableView.dataSource = self
         self.tableView.sectionIndexBackgroundColor = UIColor.clear
+        
+        self.ChooseCitySearchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.hidesNavigationBarDuringPresentation = false
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.searchBarStyle = .minimal
+            controller.searchBar.sizeToFit()
+            controller.searchBar.placeholder =  "输入城市名或拼音"
+            self.tableView.tableHeaderView = controller.searchBar
+            return controller
+        })()
     }
     
     override func didReceiveMemoryWarning() {
@@ -43,33 +72,64 @@ class ChooseCityViewController:UIViewController{
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        self.tableView.reloadData()
+    }
+}
+
+//search扩展
+extension ChooseCityViewController: UISearchResultsUpdating{
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        self.resultArray = self.cityArray.filter { (city) -> Bool in
+            return city.contains(self.ChooseCitySearchController.searchBar.text!)
+        }
+    }
 }
 
 //table view扩展
 extension ChooseCityViewController: UITableViewDelegate ,UITableViewDataSource{
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return titleArray.count
+        if(ChooseCitySearchController.isActive) {
+            return 1
+        }
+        else {
+            return titleArray.count
+        }
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section > 0 {
-            let key = titleArray[section]
-            return cityDic[key]!.count
+        if(ChooseCitySearchController.isActive) {
+            return resultArray.count
         }
-        return 1
+        else {
+            if section > 0 {
+                let key = titleArray[section]
+                return cityDic[key]!.count
+            }
+            return 1
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChooseCityTableViewCell", for: indexPath) as! ChooseCityTableViewCell
-        let key = titleArray[indexPath.section]
-        if indexPath.section == 0 {
-            cell.cityNameLabel!.text = "当前定位"
+        if(ChooseCitySearchController.isActive){
+            cell.cityNameLabel.text = self.resultArray[indexPath.row]
+            return cell
         }
         else {
-            cell.cityNameLabel.text = cityDic[key]?[indexPath.row]
+            let key = titleArray[indexPath.section]
+            if indexPath.section == 0 {
+                cell.cityNameLabel!.text = "当前定位"
+            }
+            else {
+                cell.cityNameLabel.text = cityDic[key]?[indexPath.row]
+            }
+            return cell
         }
-        return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -81,7 +141,12 @@ extension ChooseCityViewController: UITableViewDelegate ,UITableViewDataSource{
 
     //右侧导航栏
     func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return titleArray
+        if(ChooseCitySearchController.isActive) {
+            return nil
+        }
+        else {
+            return titleArray
+        }
     }
 
     //secton头
@@ -89,8 +154,13 @@ extension ChooseCityViewController: UITableViewDelegate ,UITableViewDataSource{
         let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 20))
         let title = UILabel(frame: CGRect(x: 12, y: 0, width: UIScreen.main.bounds.width - 12, height: 20))
         var titleArr = titleArray
-        titleArr[0] = "定位城市"
-        title.text = titleArr[section]
+        if(ChooseCitySearchController.isActive){
+            title.text = "查找结果"
+        }
+        else{
+            titleArr[0] = "定位城市"
+            title.text = titleArr[section]
+        }
         title.textColor = UIColor.white
         title.font = UIFont.boldSystemFont(ofSize: 15)
         view.addSubview(title)
