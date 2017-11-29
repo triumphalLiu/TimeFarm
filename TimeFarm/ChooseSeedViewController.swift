4629ba34a35dfc1ce57b3f30b46f55ba3d568b90
Liu <liu@triumphal.cn>
2017-11-29 14:16:11 +0800

    选择种子界面完成

--- TimeFarm/ChooseSeedViewController.swift
+++ TimeFarm/ChooseSeedViewController.swift
@@ -9,8 +9,56 @@
 import UIKit
 class ChooseSeedViewController:UIViewController{
     
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var unlockLabel: UILabel!
+    @IBOutlet weak var needLabel: UILabel!
+    @IBOutlet weak var chooseNext: UIButton!
+    @IBOutlet weak var chooseLast: UIButton!
+    @IBOutlet weak var setSeedPicView: UIImageView!
+    @IBOutlet weak var setTimeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTimeLabel.text = String(tomatoTime) + " : 00"
+        setSeedPicView.image = UIImage(named: seedList[currentSeedNum])
+        if(currentSeedNum == 0){
+            chooseLast.isEnabled = false
+        }else{
+            chooseLast.isEnabled = true
+        }
+        if(currentSeedNum == 2){
+            chooseNext.isEnabled = false
+        }else{
+            chooseLast.isEnabled = true
+        }
+        checkUnlock()
+    }
+    
+    private func checkUnlock(){
+        if(currentSeedNum == 0){
+            unlockLabel.text = "已解锁"
+            needLabel.text = ""
+            startButton.isEnabled = true
+        }else if(currentSeedNum == 1){
+            if(currentTomato >= 100){
+                unlockLabel.text = "已解锁"
+                needLabel.text = ""
+                startButton.isEnabled = true
+            }else{
+                unlockLabel.text = "未解锁"
+                needLabel.text = "还需" + String(100 - currentTomato) + "颗番茄即可解锁"
+                startButton.isEnabled = false
+            }
+        }else if(currentSeedNum == 2){
+            if(currentGrape >= 100){
+                unlockLabel.text = "已解锁"
+                needLabel.text = ""
+                startButton.isEnabled = true
+            }else{
+                unlockLabel.text = "未解锁"
+                needLabel.text = "还需" + String(100 - currentGrape) + "株葡萄即可解锁"
+                startButton.isEnabled = false
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -19,5 +67,30 @@ class ChooseSeedViewController:UIViewController{
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    @IBAction func clickChooseNext(_ sender: UIButton) {
+        chooseLast.isEnabled = true
+        currentSeedNum = currentSeedNum + 1
+        setSeedPicView.image = UIImage(named: seedList[currentSeedNum])
+        if(currentSeedNum == 2){
+            chooseNext.isEnabled = false
+        }
+        checkUnlock()
+    }
+    
+    @IBAction func clickChooseLast(_ sender: UIButton) {
+        chooseNext.isEnabled = true
+        currentSeedNum = currentSeedNum - 1
+        setSeedPicView.image = UIImage(named: seedList[currentSeedNum])
+        if(currentSeedNum == 0){
+            chooseLast.isEnabled = false
+        }
+        checkUnlock()
+    }
+    
+    @IBAction func clickBeginDiscount(_ sender: UIButton) {
+        isDiscountBegin = true
+        self.navigationController?.popViewController(animated: true)
     }
 }
--- TimeFarm/GlobalData.swift
+++ TimeFarm/GlobalData.swift
@@ -13,3 +13,11 @@ var tomatoTime : Int = 25 //初始番茄时间25min
 var isPaused : Bool = false //初始非暂停
 var isNotDisterb : Bool = false //初始非免打扰
 var currentCity : String = "北京" //初始城市
+
+let seedList : [String] = ["tomato.png", "grape.png", "watermelon.png"]
+var currentSeedNum : Int = 0 //默认为番茄
+var currentTomato : Int = 0
+var currentGrape : Int = 0
+var currentWaterMelon : Int = 0 //当前成就
+
+var isDiscountBegin : Bool = false //是否已经开始倒计时
